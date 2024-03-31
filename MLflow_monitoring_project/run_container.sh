91d7c0b28c80f5f2f2d72286067f3dac0bedf22f
UodessHathor <112415824+UodessHathor@users.noreply.github.com>
2024-03-31 05:24:07 +0200

    Update run_container.sh

--- MLflow_monitoring_project/run_container.sh
+++ MLflow_monitoring_project/run_container.sh
@@ -3,9 +3,6 @@ docker run -it\
  -e PORT=80\
  -e AWS_ACCESS_KEY_ID=$secret1\
  -e AWS_SECRET_ACCESS_KEY=$secret2\
- -e BACKEND_STORE_URI=nothing\
- -e ARTIFACT_STORE_URI=s3://doshdyndss-getaround-bucket/mlflow-artefacts/\
+ -e BACKEND_STORE_URI=$secret3\
+ -e ARTIFACT_STORE_URI=$secret4\
  getaround_mlflow 
-
-
-export DOCKER_DEFAULT_PLATFORM=linux/amd64
